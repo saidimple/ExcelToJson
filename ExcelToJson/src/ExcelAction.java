3207503f642625ebd13932c80b3e18e2fdd8b767
saidimple <68229716+saidimple@users.noreply.github.com>
2022-10-24 13:25:46 +0530

    Add files via upload

---
+++ ExcelToJson/src/ExcelAction.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+
+//$Id$
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 10,      // 10 MB
+        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+      )
+public class ExcelAction extends HttpServlet{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String fp = "/home/local/ZOHOCORP/sai-11750/Downloads/apache-tomcat-9.0.63/webapps/ExcelToJson/";
+        PrintWriter out = resp.getWriter();
+        out.println("output started");
+        try {
+            MultipartRequest m = new MultipartRequest(req, fp);
+        out.print("File uploaded successfully");
+        Enumeration files = m.getFileNames();
+        String fileSystemName = "";
+        while(files.hasMoreElements() ){
+          String fileName = (String)files.nextElement();
+          out.println("fn" + fileName);
+          fileSystemName= m.getFilesystemName(fileName );
+          out.println("fsn" + fileSystemName);
+         }
+        out.println(ExcelUtil.excelToJson(new File(fp + fileSystemName)));
+        } catch (Exception e) {
+            out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+    
+    
+
+}
